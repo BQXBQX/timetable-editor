45346d268ecbb4371e0214fc683d81548366c364
bqx <boqingxin14@gmail.com>
2024-07-26 21:11:39 +0800

    feat: init eventListGenerate

--- src/utils/generateEventItem.ts
+++
@@ -1,8 +0,0 @@
-import Konva from "konva";
-import { Group } from "konva/lib/Group";
-import { Event } from "../types/event";
-
-export function generateEventItem(event: Event): Group {
-  const eventGroup = new Konva.Group();
-  return eventGroup;
-}
