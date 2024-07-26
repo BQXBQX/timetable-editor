481a11b0e45adf067eac7af3ab8ee8d7872ff0ac
bqx <boqingxin14@gmail.com>
2024-07-26 21:27:41 +0800

    feat: init eventListGenerate

--- src/components/timetableEditor.tsx
+++ src/components/timetableEditor.tsx
@@ -1,7 +1,15 @@
 import Konva from "konva";
+import { Group } from "konva/lib/Group";
 import { Layer } from "konva/lib/Layer";
 import { Stage } from "konva/lib/Stage";
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { useGenerateEventItem } from "../hooks/generateEventItem";
 import { useEventsList } from "../hooks/useEventsList";
 import { type Event } from "../types/event";
 import { makeEditable } from "../utils/makeEditable";
@@ -10,28 +18,25 @@ import { ToolsBar } from "./toolsBar";
 export const TimetableEditor = () => {
   const stageRef = useRef<Stage | null>(null);
   const layerRef = useRef<Layer | null>(null);
-  const notoSansSf = useRef<FontFace | null>(null);
   const notoSansSfBold = useRef<FontFace | null>(null);
   const eventsListRef = useRef<Event[]>([]);
   const { eventsList, setEventsList } = useEventsList();
+  const [height, setHeight] = useState<Number>(0);
+  const totalGroupRef = useRef<Group | null>(null);
+  const [isHeaderLoad, setIsHeaderLoad] = useState<boolean>(false);
   eventsListRef.current = eventsList;
 
   useEffect(() => {
     stageRef.current = new Konva.Stage({
       container: "container",
       width: window.innerWidth / 2,
-      height: window.innerHeight,
+      height: 0,
     });
 
     // 创建层
     layerRef.current = new Konva.Layer();
 
     stageRef.current.add(layerRef.current);
-
-    notoSansSf.current = new FontFace(
-      "noto-sans-sc",
-      "url(./noto-sans-sc-chinese-simplified-400-normal.woff)",
-    );
 
     notoSansSfBold.current = new FontFace(
       "noto-sans-sc-bold",
@@ -41,10 +46,32 @@ export const TimetableEditor = () => {
     return () => {
       stageRef.current = null;
       layerRef.current = null;
-      notoSansSf.current = null;
       notoSansSfBold.current = null;
     };
   }, []);
+
+  useEffect(() => {
+    // 当 height 发生变化时,自动重新渲染 background
+    const backImageObj = new Image();
+    backImageObj.onload = function() {
+      const back = new Konva.Rect({
+        x: 0,
+        y: 0,
+        width: canvasWidth,
+        height: stageRef.current?.height(),
+        fillPatternImage: backImageObj,
+        fillPatternRepeat: "repeat",
+        fillPatternScaleX: 0.05,
+        fillPatternScaleY: 0.05,
+      });
+
+      totalGroupRef.current && totalGroupRef.current.add(back);
+      back.zIndex(0);
+    };
+    backImageObj.src = "./Back.png";
+
+    layerRef.current?.batchDraw();
+  }, [height]);
 
   const canvasWidth = useMemo(() => window.innerWidth / 2, []);
 
@@ -52,30 +79,14 @@ export const TimetableEditor = () => {
     // every generate will clear last generate
 
     layerRef.current?.destroyChildren();
+    stageRef.current?.height(0);
+    setIsHeaderLoad(false);
 
     const eventsList: Event[] = eventsListRef.current;
 
-    const totalGroup = new Konva.Group();
+    totalGroupRef.current = new Konva.Group();
     const headerGroup = new Konva.Group();
     const headerTitleGroup = new Konva.Group();
-
-    const backImageObj = new Image();
-    backImageObj.onload = function() {
-      const back = new Konva.Rect({
-        x: 0,
-        y: 0,
-        width: canvasWidth,
-        height: window.innerHeight,
-        fillPatternImage: backImageObj,
-        fillPatternRepeat: "repeat",
-        fillPatternScaleX: 0.05,
-        fillPatternScaleY: 0.05,
-      });
-
-      totalGroup.add(back);
-      back.zIndex(0);
-    };
-    backImageObj.src = "./Back.png";
 
     const headerImageObj = new Image();
     headerImageObj.onload = function() {
@@ -90,28 +101,24 @@ export const TimetableEditor = () => {
       });
 
       headerGroup.add(header);
+      setHeight(stageRef.current!.height() + header.height());
+      setIsHeaderLoad(true);
+      stageRef.current?.height(stageRef.current.height() + header.height());
       header.zIndex(0);
     };
     headerImageObj.src = "./Header1.png";
 
-    notoSansSf.current
-      ?.load()
-      .then((loadedFont) => {
-        document.fonts.add(loadedFont);
+    const departmentTitle = new Konva.Text({
+      x: canvasWidth * 0.05,
+      y: canvasWidth * 0.16,
+      fontSize: canvasWidth * 0.1,
+      fontFamily: "noto-sans-sc",
+      text: "软研",
+      fill: "black",
+    });
 
-        const departmentTitle = new Konva.Text({
-          x: canvasWidth * 0.05,
-          y: canvasWidth * 0.16,
-          fontSize: canvasWidth * 0.1,
-          fontFamily: "noto-sans-sc",
-          text: "软研",
-          fill: "black",
-        });
-
-        headerTitleGroup.add(departmentTitle);
-        makeEditable(departmentTitle, stageRef);
-      })
-      .catch((error) => console.log(error));
+    headerTitleGroup.add(departmentTitle);
+    makeEditable(departmentTitle, stageRef);
 
     notoSansSfBold.current
       ?.load()
@@ -134,11 +141,20 @@ export const TimetableEditor = () => {
       .catch((error) => console.log(error));
 
     headerGroup.add(headerTitleGroup);
-    totalGroup.add(headerGroup);
-    layerRef.current?.add(totalGroup);
-
+    totalGroupRef.current.add(headerGroup);
+    layerRef.current?.add(totalGroupRef.current);
     layerRef.current?.batchDraw();
   }, []);
+
+  const eventsGroupRef = useGenerateEventItem(
+    eventsListRef.current,
+    stageRef,
+    notoSansSfBold,
+    canvasWidth,
+    setHeight,
+    isHeaderLoad,
+    layerRef,
+  );
 
   return (
     <div style={{ display: "flex", flexDirection: "row", width: "100vw" }}>
